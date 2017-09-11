e25bec7bf2dee94908418cd7ffda8a8b951263cc
Hank <donghung.viethanit@gmail.com>
2017-09-11 16:16:01 +0800

    added rate_limitted and rate limit reset time

--- spec/get_response/api_spec.rb
+++ spec/get_response/api_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe GetResponse::Api do
       to_return(status: 200, body: "", headers: {})
     stub_request(:get, "https://api3.getresponse360.com/v3/campaigns").
       to_return(status: 200, body: "", headers: {})
+    stub_request(:post, "https://api3.getresponse360.com/v3/products").
+      to_return(status: 429, body: "", headers: {"x-ratelimit-reset" => "600 seconds"})
   end
 
   describe '#contacts' do
@@ -25,5 +27,12 @@ RSpec.describe GetResponse::Api do
 
   describe '#custom_fields' do
     it { expect(api.custom_fields.retrieve.success?).to be true }
+  end
+
+  describe 'limitted requests' do
+    let(:result) { api.products.create({}) }
+    it { expect(result.success?).to be false }
+    it { expect(result.rate_limitted?).to be true }
+    it { expect(result.rate_limit_reset_time).to eq 600 }
   end
 end
